70d06fc976f86f7400b60eaae2124ebf459e4690
JorgeGutierrez11 <jorge1706gutierrez@gmail.com>
2024-04-27 20:06:43 -0500

    Actualizacion2(Fin Carro)

--- 06-shopping-cart/src/components/Filters.jsx
+++ 06-shopping-cart/src/components/Filters.jsx
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
-import { useId } from 'react'
-=======
 import { useState, useId } from 'react'
->>>>>>> 4e993bac3a5ac0228d99751461b0ae5670fc1f5c
 import './Filters.css'
 import { useFilter } from '../Hooks/useFilters.js'
 
-<<<<<<< HEAD
-export function Filters() {
+export function Filters({ onChange }) {
     const {filters,setFilters} = useFilter() // Filtros
 
     const minPriceFilterId = useId()
     const categoryFilterId = useId()
 
-  /*   console.log({minPriceFilterId, categoryFilterId}) */
-=======
-export function Filters({ onChange }) {
-    const [minPrice, setMinPrice] = useState(0)
-    const minPriceFilterId = useId()
-    const categoryFilterId = useId()
-
-    console.log({minPriceFilterId, categoryFilterId})
->>>>>>> 4e993bac3a5ac0228d99751461b0ae5670fc1f5c
-
-    const handleChangeMinPrice = () => {
+    const handleChangeMinPrice = (event) => {
         setFilters(prevState => ({
             ... prevState,
             minPrice: event.target.value
